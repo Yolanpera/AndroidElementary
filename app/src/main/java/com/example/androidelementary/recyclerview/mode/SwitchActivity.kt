41f5e421f60fc5771de360031d945431675a29b4
yanpeng.p <yanpeng.p@bytedance.com>
2021-11-15 15:10:28 +0800

    try snapHelper

--- app/src/main/java/com/example/androidelementary/recyclerview/mode/SwitchActivity.kt
+++ app/src/main/java/com/example/androidelementary/recyclerview/mode/SwitchActivity.kt
@@ -1,11 +1,17 @@
-package com.example.androidelementary.recyclerview.livemode
+package com.example.androidelementary.recyclerview.mode
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.androidelementary.R
 
 class SwitchActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_switch)
+        initView()
+    }
+
+    private fun initView() {
+
     }
 }
